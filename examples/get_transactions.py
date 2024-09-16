c5b6a31828caad421d9e55ebbb09a59073299588
kesha1225 <samedov03@mail.ru>
2024-09-16 19:58:53 +0300

    Add script to fetch and print transactions using UmiPy

---
+++ examples/get_transactions.py
@@ -0,0 +1,20 @@
+import asyncio
+
+import aiohttp
+
+from umipy import UmiPy
+
+
+async def main():
+    umi = UmiPy(session=aiohttp.ClientSession(), is_legend=True, is_testnet=False)
+
+    res = await umi.get_transactions(
+        address="umistake1xhgwhyczqwphzh3zzluqch9j4uxargp6pnnn0fd9wgy7hq79jylswu0l6g",
+        limit=10,
+        offset=0
+    )
+    print(res)
+    await umi.close()
+
+
+asyncio.run(main())
