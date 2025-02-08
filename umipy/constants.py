dbe1841192b9e748186bbb8373242a5bc030d8b8
kesha1225 <samedov03@mail.ru>
2025-02-08 20:58:26 +0300

    new nodes urls

--- umipy/constants.py
+++ umipy/constants.py
@@ -1,8 +1,8 @@
 BASE_URL_ORIGINAL_MAINNET = "https://mainnet.umi.top"  # mainnet for legacy rod
 BASE_URL_LEGEND_MAINNET = "https://legend.umi.top"  # mainnet for umi legend
 
-BASE_URL_ORIGINAL_TESTNET = "https://testnet-original.umi.top"  # testnet for legacy rod
-BASE_URL_LEGEND_TESTNET = "https://testnet.umi.top"  # testnet for umi legend
+BASE_URL_ORIGINAL_TESTNET = "https://testnet.umi.top"  # testnet for legacy rod
+BASE_URL_LEGEND_TESTNET = "https://testnet-legend.umi.top"  # testnet for umi legend
 
 
 CHARSET = "qpzry9x8gf2tvdw0s3jn54khce6mua7l"
